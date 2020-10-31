b4247edc8cd5e88c055e741edc0335f5a9e4bb18
Aadam <aadam1999@gmail.com>
2020-10-31 14:18:47 +0200

    Completed Functionality.

--- covid_traffic_controller_demonstrator/client.py
+++ covid_traffic_controller_demonstrator/client.py
@@ -1,13 +1,33 @@
 from OmlaxTCP import motionSensor
 from OmlaxTCP import tempSensor
 
+
+def setup():
+    """
+    Configures two PIR motion sensors and a temperature sensor.
+    """
+    motionSensor.setup(0)
+    # Set up PIR sensor on GPIO pin 0.
+    motionSensor.setup(1)
+    # Set up PIR sensor on GPIO pin 1.
+    tempSensor.setup(1, 0x0a, 8)
+    # Establishes I2C communication between temperature sensor and RPi,
+    # on channel 1 of the pi.
+
+
 def main():
-	motionSensor.setup(0)
-	motionSensor.setup(1)
-	tempSensor.setup(1, 0x0a, 8)
-	while True:
-		tempSensor.feverScanner()
-		motionSensor.direc_detect(0, 1)
+    """
+    Determine if an individual stands infront of the temperature sensor,
+    and if so this data is sent to the server via a TCP protocol message.
+    Determines whether an individual is entering or leaving a building.
+    """
+    while True:
+        tempSensor.feverScanner()
+        # Temperature check.
+        motionSensor.direc_detect(0, 1)
+        # Direction detection check.
+
 
 if __name__ == "__main__":
-	main()
+    setup()
+    main()
--- covid_traffic_controller_demonstrator/server.py
+++ covid_traffic_controller_demonstrator/server.py
@@ -3,70 +3,135 @@ import tkFont
 import time
 from OmlaxTCP import TCP
 
-window = Tk()
-window.geometry("1920x1080")
-count = 0
-Flag = False
-fontStyle = tkFont.Font(family="Lucida Grande", size=55)
-label = Label(window, text = "There are currently \n" + str(count)+ "\n individuals inside the shopping center.", fg = "black", bg = "light blue", font = fontStyle)
-label2 = Label(window, text = "Please stand infront of the temperature sensor, \n until the screen displays either a green or red background, \n if you wish to enter.", fg = "black", bg = "light blue", font = fontStyle)
-label.pack(expand = True, fill = BOTH)
-label2.pack(expand = True, fill = BOTH)
-TCP.server_setup(5)
 
-while True:
-	window.update_idletasks()
+def main():
+    """
+    Generates GUI to illustrate number of individuals within a building
+    Listens for messages on port 1234 for updates on directional movement
+    and temperature, and further updates the GUI accordingly.
+    """
+    window = Tk()
+    # Creates a new GUI window.
+    window.geometry("1920x1080")
+    # Configures display ratio for the window.
+    count = 0
+    Flag = False
+    fontStyle = tkFont.Font(family="Lucida Grande", size=55)
+    # Configure GUI text font
+    label = Label(window, text="There are currently \n"
+                  + str(count) + "\n individuals inside the shopping center.",
+                  fg="black", bg="light blue", font=fontStyle)
+    # Creates a label to display number of individuals in building.
+    label2 = Label(window,
+                   text="Please stand infront of the temperature sensor," +
+                   "\n until the screen displays either a green or red " +
+                   "background, \n if you wish to enter.",
+                   fg="black", bg="light blue", font=fontStyle)
+    # Creates a label to display instruction the user is requested to perform.
+    label.pack(expand=True, fill=BOTH)
+    # Link the label to the GUI window.
+    label2.pack(expand=True, fill=BOTH)
+    # Link the label to the GUI window.
+    TCP.server_setup(5)
+    # Establish device as a server and open port for listening.
+
+    while True:
+        window.update_idletasks()
+        # Updates entire GUI layout and text.
         data = TCP.server_recieve(512)
-	if (data == '1') and (Flag == True):
-		Flag = False
-		label['bg'] = "light blue"
-		label2['bg'] = "light blue"
-		label2['text'] = "Please stand infront of the temperature sensor, \n until the screen displays either a green or red background, \n if you wish to enter."
-		if count == 20:
-			pass
-		else:
-			if count == 0:
-				count = count + 1
-				label['text'] = "There is currently \n" + str(count) + "\n individual inside the shopping center."
-			else:
-				count = count + 1
-				label['text'] = "There are currently \n" + str(count) + "\n individuals inside the shopping center."
-		window.update_idletasks()
+        # Retrieves message sent by client.
+        if (data == '1') and Flag:
+            Flag = False
+            label['bg'] = "light blue"
+            # Changes top half of GUI background to light blue.
+            label2['bg'] = "light blue"
+            # Changes bottom half of GUI background to light blue.
+            label2['text'] = "Please stand infront of the temperature " +
+            "sensor, \n until the screen displays either a green or " +
+            "red background, \n if you wish to enter."
+            if count == 20:
+                pass
+            else:
+                if count == 0:
+                    count = count + 1
+                    # Increment number of individuals in store.
+                    label['text'] = "There is currently \n" + str(count) +
+                    "\n individual inside the shopping center."
+                    # Changes top half text to new number in building.
+                else:
+                    count = count + 1
+                    # Increment number of individuals in store.
+                    label['text'] = "There are currently \n" + str(count) +
+                    "\n individuals inside the shopping center."
+                    # Changes top half text to new number in building.
+            window.update_idletasks()
+            # Updates entire GUI layout and text.
 
-	if data == '0':
-		label['bg'] = "light blue"
-		label2['bg'] = "light blue"
-		label2['text'] = "Please stand infront of the temperature sensor, \n until the screen displays either a green or red background, \n if you wish to enter."
+        if data == '0':
+            # Checks if message reflects an individual leaving building
+            label['bg'] = "light blue"
+            # Changes top half of GUI background to light blue.
+            label2['bg'] = "light blue"
+            # Changes bottom half of GUI background to light blue.
+            label2['text'] = "Please stand infront of the temperature " +
+            "sensor, \n until the screen displays either a green or " +
+            "red background, \n if you wish to enter."
+            if count == 0:
+                pass
+            else:
+                if count == 2:
+                    count = count - 1
+                    # Decrement number of individuals in store.
+                    label['text'] = "The is currently \n" + str(count) +
+                    "\n individual inside the shopping center."
+                    # Changes top half text to new number in building.
+                else:
+                    count = count - 1
+                    # Decrement number of individuals in store.
+                    label['text'] = "There are currently \n" + str(count) +
+                    "\n individuals inside the shopping center."
+                    # Changes top half text to new number in building.
+            window.update_idletasks()
+            # Updates entire GUI layout and text.
 
-		if count == 0:
-			pass
-		else:
-			if count == 2:
-				count = count -1
-				label['text'] = "The is currently \n" + str(count) + "\n individual inside the shopping center."
-			else:
-				count = count - 1
-				label['text'] = "There are currently \n" + str(count) + "\n individuals inside the shopping center."
-		window.update_idletasks()
+        if (data != '0') and (data != '1'):
+            # Checks if recieved message is a temperature value.
+            if (float(data) < 38):
+                # Checks if individuals temperature is regular.
+                Flag = True
+                label['bg'] = "pale green"
+                # Changes top half of GUI background to pale green.
+                label2['bg'] = "pale green"
+                # Changes bottom half of GUI background to pale green.
+                label2['text'] = "Normal temperature detected. " +
+                "\n You may proceed."
+                # Changes bottom half text to indicate normal temp scan.
+            if (float(data) >= 38):
+                # Checks if individuals temperature is above the norm.
+                label['bg'] = "salmon"
+                # Changes top half of GUI background to salmon.
+                label2['bg'] = "salmon"
+                # Changes bottom half of GUI background to salmon.
+                label2['text'] = "Above normal temperature detected. " +
+                "\n Access denied."
+                # Changes bottom half text to indicate above normal temp scan.
+                window.update_idletasks()
+                # Updates entire GUI layout and text.
+                time.sleep(5)
+                label2['text'] = "Please stand infront of the temperature " +
+                "sensor, \n until the screen displays either a green or " +
+                "red background, \n if you wish to enter."
+                # Changes bottom half text to request temperature scan.
+                label2['bg'] = "light blue"
+                # Changes top half of GUI background to light blue.
+                label['bg'] = "light blue"
+                # Changes bottom half of GUI background to light blue.
+            window.update_idletasks()
+            # Updates entire GUI layout and text.
+        print data
+        # Prints message recieved from client.
 
-	if (data != '0') and (data != '1'):
-		if (float(data) < 38):
-			Flag = True
-			label['bg'] = "pale green"
-			label2['bg'] = "pale green"
-			label2['text'] = "Normal temperature detected. \n You may proceed."
-		if (float(data) >= 38):
-			label['bg'] = "salmon"
-			label2['bg'] = "salmon"
-			label2['text'] = "Above normal temperature detected. \n Access denied."
-			window.update_idletasks()
-			time.sleep(5)
-			label2['text'] = "Please stand infront of the temperature sensor, \n until the screen displays either a green or red background, \n if you wish to enter."
-			label2['bg'] = "light blue"
-			label['bg'] = "light blue"
-		window.update_idletasks()
-	print data
 
-
-
-
+if __name__ == "__main__":
+    setup()
+    main()
